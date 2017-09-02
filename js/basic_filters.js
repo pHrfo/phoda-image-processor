ef7b48ee2618d007b8c4be8d2b549e367d733436
Darley Barreto <darleybarreto@gmail.com>
2017-09-01 22:26:59 -0300

    DEVELOP Some minor fixes

--- js/basic_filters.js
+++ js/basic_filters.js
@@ -24,6 +24,7 @@ var readImage = function(event, element) {
 };
 var genericFilter = function() {
 	var canvas = document.querySelector(".canvas")
+
 	canvas.classList.remove('hidden')
 
 	blank.width = canvas.width;
@@ -33,112 +34,130 @@ var genericFilter = function() {
 
 	if (document.getElementById("original_cb").checked == true){
 		img = document.querySelector(".image-container .img")
-		document.getElementById('originalh').innerHTML = "Usando imagem original"
+		if (img.src)
+			document.getElementById('originalh').innerHTML = "Using original image"
 	}
 	else{
 		buffered_img.src = canvas.toDataURL();
       	if (buffered_img.src == blank.toDataURL()){
       		img = document.querySelector(".image-container .img")
-      		document.getElementById('originalh').innerHTML = "Usando imagem original"
+      		if (img.src)
+      			document.getElementById('originalh').innerHTML = "Using original image"
       	}
       	else{
       		img = buffered_img;
-      		document.getElementById('originalh').innerHTML = "Usando última imagem editada"
+
+      		if (img.src)
+      			document.getElementById('originalh').innerHTML = "Using last edited image"
       	}  	
 	}
 
-	canvas.width  = img.width;
-	canvas.height = img.height;
+	if (img.src){
 
-	var ctx = canvas.getContext("2d")
-
-	ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+		canvas.width  = img.width;
+		canvas.height = img.height;
 	
-	return ctx.getImageData(0, 0, canvas.width, canvas.height)
+		var ctx = canvas.getContext("2d")
+	
+		ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+		
+		return ctx.getImageData(0, 0, canvas.width, canvas.height)
+	}
 };
 
 var blackAndWhite = function() {
 	var imgData = genericFilter()
-
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		var mean = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2])/3
-		imgData.data[i] = mean
-		imgData.data[i+1] = mean
-		imgData.data[i+2] = mean
+	if(imgData){
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			var mean = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2])/3
+			imgData.data[i] = mean
+			imgData.data[i+1] = mean
+			imgData.data[i+2] = mean
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 };
 
 var negativeBW = function() {
 	var imgData = genericFilter()
-
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		var mean = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2])/3
-		imgData.data[i] = 255 - mean
-		imgData.data[i+1] = 255 - mean
-		imgData.data[i+2] = 255 - mean
+	if(imgData){
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			var mean = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2])/3
+			imgData.data[i] = 255 - mean
+			imgData.data[i+1] = 255 - mean
+			imgData.data[i+2] = 255 - mean
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 };
 
 var negative = function() {
 	var imgData = genericFilter()
-
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		imgData.data[i] = 255 - imgData.data[i]
-		imgData.data[i+1] = 255 - imgData.data[i+1]
-		imgData.data[i+2] = 255 - imgData.data[i+2]
+	if(imgData){
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i] = 255 - imgData.data[i]
+			imgData.data[i+1] = 255 - imgData.data[i+1]
+			imgData.data[i+2] = 255 - imgData.data[i+2]
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 };
 
 var log = function() {
 	var imgData = genericFilter()
-
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		imgData.data[i] = 46*Math.log(1 + imgData.data[i])
-		imgData.data[i+1] = 46*Math.log(1 + imgData.data[i+1])
-		imgData.data[i+2] = 46*Math.log(1 + imgData.data[i+2])
+	if(imgData){
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i] = 46*Math.log(1 + imgData.data[i])
+			imgData.data[i+1] = 46*Math.log(1 + imgData.data[i+1])
+			imgData.data[i+2] = 46*Math.log(1 + imgData.data[i+2])
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 };
 
 var exp = function() {
 	var imgData = genericFilter()
-
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		imgData.data[i] = Math.exp(imgData.data[i])
-		imgData.data[i+1] = Math.exp(imgData.data[i+1])
-		imgData.data[i+2] = Math.exp(imgData.data[i+2])
+	if(imgData){
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i] = Math.exp(imgData.data[i])
+			imgData.data[i+1] = Math.exp(imgData.data[i+1])
+			imgData.data[i+2] = Math.exp(imgData.data[i+2])
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 };
 
 var threshold = function() {
 	var imgData = genericFilter()
-	var thresh = document.getElementById("thresholdValue").value
 
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		var mean = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2])/3
-		imgData.data[i] = (mean > thresh ? 0 : 255)
-		imgData.data[i+1] = (mean > thresh ? 0 : 255)
-		imgData.data[i+2] = (mean > thresh ? 0 : 255)
+	if(imgData){
+		var thresh = document.getElementById("thresholdValue").value
+
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			var mean = (imgData.data[i] + imgData.data[i+1] + imgData.data[i+2])/3
+			imgData.data[i] = (mean > thresh ? 0 : 255)
+			imgData.data[i+1] = (mean > thresh ? 0 : 255)
+			imgData.data[i+2] = (mean > thresh ? 0 : 255)
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
-}
+};
 
 var gamma = function(){
 	var imgData = genericFilter()
-	var cons = document.getElementById("gammaConst").value
-	var gamma = document.getElementById("gammaRange").value
 
-	for (var i = 0; i < imgData.data.length; i += 4) {
-		imgData.data[i] = cons*Math.pow(imgData.data[i],gamma)
-		imgData.data[i+1] = cons*Math.pow(imgData.data[i+1],gamma)
-		imgData.data[i+2] = cons*Math.pow(imgData.data[i+2],gamma)
+	if(imgData){
+		var cons = document.getElementById("gammaConst").value
+		var gamma = document.getElementById("gammaRange").value
+		
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i] = cons*Math.pow(imgData.data[i],gamma)
+			imgData.data[i+1] = cons*Math.pow(imgData.data[i+1],gamma)
+			imgData.data[i+2] = cons*Math.pow(imgData.data[i+2],gamma)
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
 	}
-	document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
-}
+};
 
 var gamma_range = function(value){
 	var image = document.querySelector('.image-container .img')
@@ -147,14 +166,35 @@ var gamma_range = function(value){
 	}
 	document.getElementById("valuegamma").value = value;
 
-}
+};
+
+var bitPlaneSlicing = function (value) {
+	var imgData = genericFilter()
+	if(imgData){
+		for (var i = 0; i < imgData.data.length; i += 4) {
+			imgData.data[i] = imgData.data[i]&Math.pow(imgData.data[i],value)
+			imgData.data[i+1] = imgData.data[i+1]&Math.pow(imgData.data[i],value)
+			imgData.data[i+2] = imgData.data[i+2]&Math.pow(imgData.data[i],value)
+		}
+		document.querySelector(".canvas").getContext("2d").putImageData(imgData, 0, 0)
+	}
+};
 
 var showFilters = function () {
 	window.opt = 1;
 	var imageContainer = document.querySelector('.filter-container')
 	var histogramContainer = document.querySelector('.histogram-container')
+	
+	Object.assign(document.querySelector('.histogram-container').style,{display:"none"});
+
+
+	if (!histogramContainer.classList.contains('hidden')){
+		histogramContainer.classList.add('hidden')
+		document.querySelector('.chart-div').classList.add('hidden')
+	}
 
 	if (imageContainer.classList.contains('hidden')){
 		imageContainer.classList.remove('hidden')
 	}
+
 }
--- js/histogram.js
+++ js/histogram.js
@@ -33,29 +33,75 @@ var showHistogram = function () {
 	var histogramContainer = document.querySelector('.histogram-container')
 	var imageContainer = document.querySelector('.filter-container')
 
+	Object.assign(document.querySelector('.histogram-container').style,{display:"block"});
+
 	if (!imageContainer.classList.contains('hidden')){
 		imageContainer.classList.add('hidden')
+		document.querySelector('.canvas').classList.add("hidden")
 	} 
 	if (histogramContainer.classList.contains('hidden')){
 		histogramContainer.classList.remove('hidden')
 	}
 
-	var canvas = document.createElement('canvas');
-	var ctx = canvas.getContext("2d")
-	img = document.querySelector(".image-container .img")
-	ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
-	
-	var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-
-	var data = [["Color"]]
-	var histogram = {}
-	for (var i = 0; i < imageData.data.length; i += 4) {
-		var mean = (imageData.data[i] + imageData.data[i+1] + imageData.data[i+2])/3
-		data.push([Math.floor(mean)])
-	}
-
-	drawChart(data)
 }
 
+var greyHistogram = function () {
+	
+	img = document.querySelector(".image-container .img")
 
+	if (img.src){
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext("2d")
 
+		ctx.drawImage(img, 0, 0, canvas.width, canvas.height)
+		
+		var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+
+		var data = [["Color"]]
+		var histogram = {}
+		for (var i = 0; i < imageData.data.length; i += 4) {
+			var mean = (imageData.data[i] + imageData.data[i+1] + imageData.data[i+2])/3
+			data.push([Math.floor(mean)])
+		}
+		drawChart(data)
+	}
+}
+
+var globalHistogramEq = function() {
+	var img = document.querySelector(".image-container .img")
+	if(img){
+	    var srcLength = img.src.length;
+	    var src = img.src
+	    var dst = src;
+
+	    // Compute histogram and histogram sum:
+	    var hist = new Float32Array(256);
+	    var sum = 0;
+	    for (var i = 0; i < srcLength; ++i) {
+	        ++hist[~~src[i]];
+	        ++sum;
+	    }
+
+	    // Compute integral histogram:
+	    var prev = hist[0];
+	    for (var i = 1; i < 256; ++i) {
+	        prev = hist[i] += prev;
+	    }
+
+	    // Equalize image:
+	    var norm = 255 / sum;
+	    for (var i = 0; i < srcLength; ++i) {
+	        dst[i] = hist[~~src[i]] * norm;
+	    }
+	    
+	    var canvas = document.querySelector(".canvas")
+	    var ctx = canvas.getContext("2d")
+		
+		data = ctx.getImageData(0, 0, canvas.width, canvas.height)
+		data.data.set(new Uint8ClampedArray(dst));
+
+	    ctx.putImageData(data, 0, 0)
+	    if (document.querySelector(".canvas").classList.contains("hidden"))
+	    	document.querySelector(".canvas").classList.remove("hidden")
+	}
+}
